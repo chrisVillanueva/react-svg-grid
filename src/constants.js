df4a727a4c7aa5282bf6d9198e3678f3831e9bf4
chrisVillanueva <chrisvillanuevanyc@gmail.com>
2020-02-27 22:35:17 -0500

    update: wallButtonLabel values

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,5 @@ export const RECT_STYLE_OBJ = {
   stroke: '#222'
 };
 export const RECT_ACTIVE_NODE_FILL = '#585858';
+export const START_BLOCKER = 'Start Blocker';
+export const END_BLOCKER = 'Stop Blocker';
